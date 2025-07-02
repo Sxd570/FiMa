873ac2c04ef082e9c64592a1887ef6c91b19cb2b
Sxd570 <b.salman13001@gmail.com>
2025-07-02 19:48:24 +0530

    Refactor Budget and Category models; remove unused fields and update constructors

--- api/core/models/tables/budget.py
+++ api/core/models/tables/budget.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -7,16 +7,16 @@ class Budget(Base):
     __tablename__ = 'budget'
     
     user_id = Column(String(36), nullable=False)
+    category_id = Column(String(36), nullable=False)
     budget_id = Column(String(36), primary_key=True, nullable=False)
-    budget_name = Column(String(255), nullable=False)
-    budget_description = Column(String(255), nullable=True)
-    budget_target_amount = Column(Integer, nullable=False)
-    budget_current_amount = Column(Integer, default=0, nullable=False)
+    allocated_amount = Column(Integer, nullable=False)
+    spent_amount = Column(Integer, nullable=False)
+    is_limit_reached = Column(Boolean, nullable=False)
 
-    def __init__(self, user_id, budget_id, budget_name, budget_description, budget_current_amount, budget_target_amount):
+    def __init__(self, user_id, category_id, budget_id, allocated_amount, spent_amount, is_limit_reached):
         self.user_id = user_id
+        self.category_id = category_id
         self.budget_id = budget_id
-        self.budget_name = budget_name
-        self.budget_description = budget_description
-        self.budget_current_amount = budget_current_amount
-        self.budget_target_amount = budget_target_amount
+        self.allocated_amount = allocated_amount
+        self.spent_amount = spent_amount
+        self.is_limit_reached = is_limit_reached
