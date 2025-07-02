99502d49770019e116df1fa0eec5e86cffccfb42
Sxd570 <b.salman13001@gmail.com>
2025-07-02 20:10:18 +0530

    Refactor Goal and Transactions models; add is_goal_reached field and rename transaction fields for clarity

--- api/core/models/tables/goal.py
+++ api/core/models/tables/goal.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,12 +12,14 @@ class Goal(Base):
     goal_description = Column(String(255), nullable=True)
     goal_target_amount = Column(Integer, nullable=False)
     goal_current_amount = Column(Integer, default=0, nullable=False)
+    is_goal_reached = Column(Boolean, default=False, nullable=False)
 
-    def __init__(self, user_id, goal_id, goal_name, goal_description, goal_current_amount, goal_target_amount):
+    def __init__(self, user_id, goal_id, goal_name, goal_description, goal_current_amount, goal_target_amount, is_goal_reached):
         self.user_id = user_id
         self.goal_id = goal_id
         self.goal_name = goal_name
         self.goal_description = goal_description
         self.goal_current_amount = goal_current_amount
         self.goal_target_amount = goal_target_amount
+        self.is_goal_reached = is_goal_reached
 
