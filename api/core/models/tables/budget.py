5aa0d29280f5bd0084d58ef1e7b59002eb71a1ae
Sxd570 <b.salman13001@gmail.com>
2025-07-02 21:04:36 +0530

    Update Budget model to include allocated_month in constructor and adjust initialization

--- api/core/models/tables/budget.py
+++ api/core/models/tables/budget.py
@@ -11,12 +11,14 @@ class Budget(Base):
     budget_id = Column(String(36), primary_key=True, nullable=False)
     allocated_amount = Column(Integer, nullable=False)
     spent_amount = Column(Integer, nullable=False)
+    allocated_month = Column(String(7), nullable=False)
     is_limit_reached = Column(Boolean, nullable=False)
 
-    def __init__(self, user_id, category_id, budget_id, allocated_amount, spent_amount, is_limit_reached):
+    def __init__(self, user_id, category_id, budget_id, allocated_amount, spent_amount, allocated_month, is_limit_reached):
         self.user_id = user_id
         self.category_id = category_id
         self.budget_id = budget_id
         self.allocated_amount = allocated_amount
         self.spent_amount = spent_amount
+        self.allocated_month = allocated_month  
         self.is_limit_reached = is_limit_reached
