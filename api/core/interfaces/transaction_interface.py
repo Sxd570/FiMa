eb1dddf2a21f6c11ff5c1672cc61820983532090
Sxd570 <b.salman13001@gmail.com>
2025-07-10 23:11:01 +0530

    Define abstract method for transaction retrieval in TransactionInterface

--- api/core/interfaces/transaction_interface.py
+++ api/core/interfaces/transaction_interface.py
@@ -2,4 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class TransactionInterface(ABC):
-    pass
+    @abstractmethod
+    def get_transactions(self, db_request):
+        """
+        Fetch transactions based on the provided request.
+        
+        :param db_request: An instance of GetTransactionDBRequest containing user ID and filters.
+        :return: A list of transactions matching the request criteria.
+        """
+        pass
