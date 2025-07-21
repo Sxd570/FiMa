fa375a886a1c8ddd079b2707b1bfd4dfb3e93426
Sxd570 <b.salman13001@gmail.com>
2025-07-21 22:31:25 +0530

    Add user_password field to user table in create_tables and user.sql

--- repo/alembic/alembic/versions/a249f5e81f4b_create_tables.py
+++ repo/alembic/alembic/versions/a249f5e81f4b_create_tables.py
@@ -23,7 +23,8 @@ def create_user_table():
     CREATE TABLE `user` (
         `user_id` VARCHAR(36) PRIMARY KEY NOT NULL,
         `user_name` VARCHAR(255) NOT NULL,
-        `user_email` VARCHAR(255) NOT NULL UNIQUE
+        `user_email` VARCHAR(255) NOT NULL UNIQUE,
+        `user_password` VARCHAR(255) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
     ''')
 
