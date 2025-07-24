7206c1ceacb396ee6023c8073b125badf79b3e32
Sxd570 <b.salman13001@gmail.com>
2025-07-24 11:52:52 +0530

    Refactor Boto3 client initialization: remove region dependency and delete unused Cognito class for improved code clarity and maintainability.

--- api/infrastructure/aws/boto3/__init__.py
+++ api/infrastructure/aws/boto3/__init__.py
@@ -6,13 +6,11 @@ logger = Logger(__name__)
 class Boto3:
     def __init__(self):
         self.endpoint_url = "http://localhost:4566"
-        self.region = "us-east-1"
 
     def client(self, service_name: str):
         try:
             return boto3.client(
                 service_name,
-                region_name=self.region,
                 endpoint_url=self.endpoint_url
             )
         except Exception as e:
