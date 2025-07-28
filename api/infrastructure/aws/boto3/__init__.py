832368569399cff4b512192892d5dca72bb4e6b2
SalmanMC <baba.salman@montycloud.com>
2025-07-28 21:02:00 +0530

    Refactor Boto3 client initialization: remove unused endpoint URL and streamline client method for improved clarity and maintainability.

--- api/infrastructure/aws/boto3/__init__.py
+++ api/infrastructure/aws/boto3/__init__.py
@@ -5,15 +5,14 @@ logger = Logger(__name__)
 
 class Boto3:
     def __init__(self):
-        # self.endpoint_url = "http://localhost:4566"
-        ...
+        self.session = None
 
     def client(self, service_name: str):
         try:
-            return boto3.client(
+            self.session = boto3.client(
                 service_name
-                # endpoint_url=self.endpoint_url
             )
+            return self.session
         except Exception as e:
             logger.error(f"Error creating boto3 client for {service_name}: {e}")
-            raise
+            raise e
