23061ab082bc03bb736e2ce8de4528fc8b267e5b
Sxd570 <b.salman13001@gmail.com>
2025-08-28 22:10:37 +0530

    Refactor API request handling and database configuration; remove unused AWS service files and update .env for environment variables.

--- api/shared/Utility/api_request.py
+++ api/shared/Utility/api_request.py
@@ -1,27 +1,69 @@
 import json
 import requests
 from shared.logger import Logger
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logger = Logger(__name__)
 
 
-def api_request(http_method, path, query_params):
-    """
-    Prepares and sends an API request.
-    """
-    url = 
-    headers = {"Content-Type": "application/json"}
-    try:
-        if http_method == "GET":
-            response = requests.get(url, headers=headers, params=query_params)
-        elif http_method == "POST":
-            response = requests.post(url, headers=headers, json=query_params)
-        else:
-            logger.error(f"Unsupported HTTP method: {http_method}")
-            return {"error": "Unsupported HTTP method"}, 405
+class APIRequest:
+    def __init__(self, http_method, endpoint, payload):
+        self.http_method = http_method
+        self.endpoint = endpoint
+        self.payload = payload
+        self.url = self.generate_url(endpoint)
+        self.headers = {"Content-Type": "application/json"}
 
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        logger.error(f"Error occurred while making API request: {e}")
-        return {"error": str(e)}, 500
+    def generate_url(self, endpoint):
+        api = "api"
+        
+        base_url = os.getenv("API_BASE_URL")
+        if not base_url:
+            logger.error("API_BASE_URL is not set in environment variables.")
+            raise ValueError("API_BASE_URL is not set in environment variables.")
+        
+        port = os.getenv("API_PORT")
+        if not port:
+            logger.error("API_PORT is not set in environment variables.")
+            raise ValueError("API_PORT is not set in environment variables.")
+        
+        version = os.getenv("API_VERSION")
+        if not version:
+            logger.error("API_VERSION is not set in environment variables.")
+            raise ValueError("API_VERSION is not set in environment variables.")
+
+        return f"{base_url}:{port}/{api}/{version}/{endpoint}"
+
+    def execute(self):
+        try:
+            ...
+        except Exception as e:
+            logger.error(f"Error occurred while making API request: {e}")
+            return {"error": str(e)}
+
+
+# def get
+
+# def api_request(http_method, endpoint, query_params):
+#     """
+#     Prepares and sends an API request.
+#     """
+#     url = f"{API_BASE_URL}:{API_PORT}/{endpoint}"
+#     headers = {"Content-Type": "application/json"}
+#     try:
+#         if http_method == "GET":
+#             response = requests.get(url, headers=headers, params=query_params)
+#         elif http_method == "POST":
+#             response = requests.post(url, headers=headers, json=query_params)
+#         else:
+#             logger.error(f"Unsupported HTTP method: {http_method}")
+#             return {"error": "Unsupported HTTP method"}, 405
+
+#         response.raise_for_status()
+#         return response.json()
+#     except requests.RequestException as e:
+#         logger.error(f"Error occurred while making API request: {e}")
+#         return {"error": str(e)}, 500
--- api/shared/Utility/db_config.py
+++ api/shared/Utility/db_config.py
@@ -1,29 +1,13 @@
-from services.aws.secret_manager import SecretManager
-from shared.logger import Logger
+import os
+from dotenv import load_dotenv
 
-logger = Logger(__name__)
-
-FIMA_DB_SECRET = "fima-db-creds"
-
-def get_db_path():
-    return FIMA_DB_SECRET
-
-def db_config():
-    path = get_db_path()
-    try:
-        secret_manager_session = SecretManager()
-        db_config = secret_manager_session.get_secret(path)
-        return db_config
-    except Exception as e:
-        logger.error(f"Error retrieving database configuration: {e}")
-        raise e
-    
+load_dotenv()
 
 def get_db_config():
     return {
-        "user": "fima",
-        "password": "Fima1234!",
-        "host": "localhost",
-        "port": 3306,
-        "database": "fima"
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD"),
+        "host": os.getenv("DB_HOST"),
+        "port": os.getenv("DB_PORT"),
+        "database": os.getenv("DB_NAME")
     }
